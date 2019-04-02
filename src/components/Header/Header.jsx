0fb80ef077f6dd8afdb7db4e25fcd7c4b78d7f4d
Adam Kopczynski <akadamkopczynski@gmail.com>
2019-04-02 14:41:13 +0200

    Init v2 branch and create react project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <header className="page-header">
+            <div className="row">
+            
+            </div>
+        </header>
+    )
+}
+
+export default Header;
