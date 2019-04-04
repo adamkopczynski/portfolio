a535734cde36cc8f61e484978314445e364bde40
Adam Kopczynski <akadamkopczynski@gmail.com>
2019-04-04 21:12:57 +0200

    Generate build

---
+++ build/precache-manifest.afa0fb94ccc1feeb7e671d3825e4254e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/static/media/react.692adb35.png"
+  },
+  {
+    "revision": "48a1e17d20308bf6f1b1",
+    "url": "/static/css/main.6f742315.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "71ddbac6b15782d89211c6b7a3938ebf",
+    "url": "/static/media/agile.71ddbac6.svg"
+  },
+  {
+    "revision": "27bf9f5274f2a9b51d1e",
+    "url": "/static/js/2.3caf5584.chunk.js"
+  },
+  {
+    "revision": "48a1e17d20308bf6f1b1",
+    "url": "/static/js/main.1bd4dbd4.chunk.js"
+  },
+  {
+    "revision": "333efbeb24b25a3e6e47448160817634",
+    "url": "/static/media/Adam Kopczynski - CV.333efbeb.pdf"
+  },
+  {
+    "revision": "5a1a70f37f40a6e878456b0a9c290cb9",
+    "url": "/static/media/code.5a1a70f3.svg"
+  },
+  {
+    "revision": "ad3f2a7783b99b0a99956bc7e0fb4b8b",
+    "url": "/static/media/managment.ad3f2a77.svg"
+  },
+  {
+    "revision": "27bf9f5274f2a9b51d1e",
+    "url": "/static/css/2.5e328f69.chunk.css"
+  },
+  {
+    "revision": "cb447b21e0f83745ce6215e956f3b967",
+    "url": "/index.html"
+  }
+];
