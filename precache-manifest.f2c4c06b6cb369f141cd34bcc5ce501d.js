daed9e343bbebdc8002862cad871666f25ef05f5
adamkopczynski <akadamkopczynski@gmail.com>
2020-05-29 14:33:03 +0200

    Updates

---
+++ precache-manifest.f2c4c06b6cb369f141cd34bcc5ce501d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47d16a57d4a5ccd62ad8944bea83dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f0c96def331c99b7c6",
+    "url": "/static/css/2.adea9080.chunk.css"
+  },
+  {
+    "revision": "214585c76280da265f58",
+    "url": "/static/css/main.53cd5d1c.chunk.css"
+  },
+  {
+    "revision": "77f0c96def331c99b7c6",
+    "url": "/static/js/2.6b4c9643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b4c9643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214585c76280da265f58",
+    "url": "/static/js/main.01dcc4e4.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "2351a191d5980470ec3b9d16ef54925b",
+    "url": "/static/media/Adam Kopczynski - CV.2351a191.pdf"
+  },
+  {
+    "revision": "71ddbac6b15782d89211c6b7a3938ebf",
+    "url": "/static/media/agile.71ddbac6.svg"
+  },
+  {
+    "revision": "2d48c7208aa82d424041d02c9ff54f80",
+    "url": "/static/media/chat.2d48c720.jpg"
+  },
+  {
+    "revision": "5a1a70f37f40a6e878456b0a9c290cb9",
+    "url": "/static/media/code.5a1a70f3.svg"
+  },
+  {
+    "revision": "ad3f2a7783b99b0a99956bc7e0fb4b8b",
+    "url": "/static/media/managment.ad3f2a77.svg"
+  },
+  {
+    "revision": "26289345aac26526252dd4213cbe629a",
+    "url": "/static/media/me.26289345.jpg"
+  },
+  {
+    "revision": "5423a7fd664ce29c04b0e5f106386e98",
+    "url": "/static/media/pc.5423a7fd.jpg"
+  },
+  {
+    "revision": "4eb774f9b3b432288968e5096d7c6ff6",
+    "url": "/static/media/poli.4eb774f9.jpg"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/static/media/react.692adb35.png"
+  },
+  {
+    "revision": "641c47aa75f56ab2b0918653d4eff9c7",
+    "url": "/static/media/yt.641c47aa.jpg"
+  }
+]);
